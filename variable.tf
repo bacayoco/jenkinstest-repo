bc983f3b69ecd60ced0acb98bbb67d95181a606d
Banji Azeh <85324803+bacayoco@users.noreply.github.com>
2023-04-04 23:49:08 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,17 @@
-variable "key_name" {
+variable "vpc_cidr" {
   type        = string
-  description = "name of keypair to pull down using data source"
+  description = "vpc network cidr address for kojitechs vpc"
+  default = "10.0.0.0/16"
+}
 
-  default     = "10.0.0.0/16"
+variable "public_subnet_cidr" {
+  type        = list(any)
+  description = "List of public subnet"
+  default = ["10.0.0.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet" {
+  type        = list(any)
+  description = "List of public subnet"
+  default = ["10.0.5.0/24", "10.0.7.0/24"]
 }
