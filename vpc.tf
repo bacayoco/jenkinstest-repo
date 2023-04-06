7d42dcb2019b01602a8be7e1f0f6e7979cfa2952
grandbaca <azeh123tanket@gmail.com>
2023-04-05 21:26:26 -0700

    new changes

--- vpc.tf
+++ vpc.tf
@@ -9,16 +9,16 @@
 resource "aws_vpc" "demo_vpc" {
   cidr_block = "10.0.0.0/16"
 
-    tags = {
+  tags = {
     Name = "terraform_demo"
   }
 
 }
-resource "aws_internet_gateway" "demo_vpc-IGW" {  
-      # Creating Internet Gateway
-    vpc_id =  aws_vpc.demo_vpc.id 
+resource "aws_internet_gateway" "demo_vpc-IGW" {
+  # Creating Internet Gateway
+  vpc_id = aws_vpc.demo_vpc.id
 
-        tags = {
+  tags = {
     Name = "terraform_demo"
   }
 }
@@ -33,13 +33,13 @@ resource "aws_route_table_association" "RT-association" {
   subnet_id      = aws_subnet.priv_subnet-1.id
   route_table_id = aws_route_table.RT.id
 
- 
+
 }
 
 resource "aws_route" "pub-RT" {
-  route_table_id              = aws_route_table.RT.id
+  route_table_id         = aws_route_table.RT.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id       = aws_internet_gateway.demo_vpc-IGW.id
+  gateway_id             = aws_internet_gateway.demo_vpc-IGW.id
 
 
 }
@@ -55,29 +55,29 @@ resource "aws_route_table" "RT" {
 
 
 resource "aws_subnet" "priv_subnet-1" {
-  vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
-    tags = {
+  tags = {
     Name = "priv_subnet-1"
   }
 }
 
 resource "aws_subnet" "pub_subnet-1" {
-  vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 
-    tags = {
+  tags = {
     Name = "pub_subnet"
   }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ami.id
   instance_type = "t3.micro"
-  subnet_id   = aws_subnet.pub_subnet-1.id
+  subnet_id     = aws_subnet.pub_subnet-1.id
 
   tags = {
     Name = "terraform_demo"
